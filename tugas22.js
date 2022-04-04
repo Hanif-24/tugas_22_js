f0c88cd753cbeac19b91232dbea6f4ac7c2debe6
Hanif-Niomic <m.alihanif249@gmail.com>
2022-04-04 21:44:19 +0700

    tugas java
    tugas java

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function tugas22(){
+  var kata = "Saya ingin belajar bersama";
+  var ubahkata = kata.split(" ");
+  ubahkata.forEach(function(x,y){
+    console.log("Item :",x,"Index ke ",y);
+  })
+}
+tugas22()
